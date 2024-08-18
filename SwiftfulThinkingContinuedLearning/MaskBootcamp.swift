b8f2805a2f2e35f973a54a9b44654b51fc115aad
sapere4ude <eileenyoo2@gmail.com>
2024-08-18 23:25:40 +0900

    - use mask() modifier

--- SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -7,14 +7,42 @@
 
 import SwiftUI
 
+/// mask 사용에 대한 공식 문서의 예제
+///   Image(systemName: "envelope.badge.fill")
+///    .foregroundColor(Color.blue)
+///    .font(.system(size: 128, weight: .regular))
+///    .mask(Rectangle().opacity(0.1))
+
 struct MaskBootcamp: View {
     
-    @State var rating: Int = 3
+    @State var rating: Int = 0
     
     var body: some View {
         ZStack {
             starsView
+                .overlay(
+                    overlayView
+                        .mask(starsView)
+                )
         }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    //.foregroundColor(.yellow)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.yellow, Color.pink]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
     }
     
     private var starsView: some View {
@@ -22,9 +50,12 @@ struct MaskBootcamp: View {
             ForEach(1..<6) { index in
                 Image(systemName: "star.fill")
                     .font(.largeTitle)
-                    .foregroundColor(rating >= index ? Color.yellow : .gray)
+                    //.foregroundColor(rating >= index ? Color.yellow : .gray)
+                    .foregroundColor(Color.gray)
                     .onTapGesture {
-                        rating = index
+                        withAnimation(.easeOut) {
+                            rating = index
+                        }
                     }
             }
         }
