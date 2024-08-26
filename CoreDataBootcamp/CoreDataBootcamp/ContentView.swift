3a78ab56938390a2381e63628d9b57302622824f
sapere4ude <eileenyoo2@gmail.com>
2024-08-26 20:08:48 +0900

    - How to use Core Data With @FetchRequest in SwiftUI

---
+++ CoreDataBootcamp/CoreDataBootcamp/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  CoreDataBootcamp
+//
+//  Created by Kant on 8/26/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: FruitEntity.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)])
+    var fruits: FetchedResults<FruitEntity>
+    
+    @State var textFieldText: String = ""
+
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                TextField("Add fruit here...", text: $textFieldText)
+                    .font(.headline)
+                    .padding(.leading)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                
+                Button(action: {
+                    addItem()
+                }, label: {
+                    Text("Submit")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal)
+                
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                            .onTapGesture {
+                                updateItem(fruit: fruit)
+                            }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("Fruits")
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newFruit = FruitEntity(context: viewContext)
+            newFruit.name = textFieldText
+            saveItems()
+            textFieldText = ""
+        }
+    }
+    
+    private func updateItem(fruit: FruitEntity) {
+        withAnimation {
+            let currentName = fruit.name ?? ""
+            let newName = currentName + "!"
+            fruit.name = newName
+            saveItems()
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            guard let index = offsets.first else { return }
+            let fruitEntity = fruits[index]
+            viewContext.delete(fruitEntity)
+            saveItems()
+        }
+    }
+    
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
---
+++ CoreDataBootcamp/CoreDataBootcampTests/CoreDataBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootcampTests.swift
+//  CoreDataBootcampTests
+//
+//  Created by Kant on 8/26/24.
+//
+
+import Testing
+@testable import CoreDataBootcamp
+
+struct CoreDataBootcampTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
